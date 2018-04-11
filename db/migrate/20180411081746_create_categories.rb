ee7feec76fa2e8cdc909e3c375b5739f0e2e01fc
Alexei Shilin <alexei.shilin@gmail.com>
2018-04-11 16:07:47 +0700

    add Category model

---
+++ db/migrate/20180411081746_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :categories do |t|
+      t.string :name, null: false
+      t.integer :products_count, default: 0
+
+      t.timestamps
+    end
+    add_index :categories, :name, unique: true
+  end
+end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category do
+    name "MyCategoryName"
+    products_count 0
+  end
+end
